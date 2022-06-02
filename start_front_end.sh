99cecafe6215fa62ade5dbc98f392bc80d57c79b
Brian Wilson <brian@wildsong.biz>
2022-06-02 16:36:32 -0700

    checkpt

--- start_front_end.sh
+++ start_front_end.sh
@@ -1,9 +1,14 @@
 cd /srv/app
 
-CERT="/srv/certs/$1/fullchain.pem"
-KEY="/srv/certs/$1/key.pem"
+#CERT="/srv/certs/$1/fullchain.pem"
+#KEY="/srv/certs/$1/key.pem"
+
+CERT=/srv/certs/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$1/$1.crt
+KEY=/srv/certs/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$1/$1.key
 
 npm install
+
+ls -l $CERT $KEY
 
 parcel serve --cert=$CERT --key=$KEY --host=0.0.0.0 index.html
 
