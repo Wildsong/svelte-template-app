2b2e3e62ac0d6851b78ef8796a76f97444aaa2d8
bwilsoncc <bwilsoncc@co.clatsop.or.us>
2022-06-02 17:01:19 -0700

    chkpt

--- start_front_end.sh
+++ start_front_end.sh
@@ -3,12 +3,13 @@ cd /srv/app
 #CERT="/srv/certs/$1/fullchain.pem"
 #KEY="/srv/certs/$1/key.pem"
 
-CERT=/srv/certs/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$1/$1.crt
-KEY=/srv/certs/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$1/$1.key
+#CERT=/srv/certs/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$1/$1.crt
+#KEY=/srv/certs/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$1/$1.key
 
 npm install
 
 ls -l $CERT $KEY
 
-parcel serve --cert=$CERT --key=$KEY --host=0.0.0.0 index.html
+#parcel serve --cert=$CERT --key=$KEY --host=0.0.0.0 index.html
+parcel serve --host=0.0.0.0 index.html
 
